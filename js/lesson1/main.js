3ab3f4ba7478f907aca9e82177a3578940ef18df
Zoella7 <yuliahavrylyshyn1@gmail.com>
2022-04-14 14:12:10 +0300

    add

---
+++ js/lesson1/main.js
@@ -0,0 +1,121 @@
+
+// // /*First part*/
+// let a = 'hello';
+// console.log(a);
+// // alert(a);
+// // document.write(a);
+//
+// let b = 'owu';
+// console.log(b);
+// // alert(b);
+// // document.write(b);
+//
+// let c = 'com';
+// console.log(c);
+// // alert(c);
+// // document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// // alert(d);
+// // document.write(d);
+//
+// let e = 1;
+// console.log(1);
+// // alert(e);
+// // document.write(e);
+//
+// let f = 10;
+// console.log(f);
+// // alert(f);
+// // document.write(f);
+//
+// let g = -999;
+// console.log(g);
+// // alert(g);
+// // document.write(g);
+//
+// let h = 123;
+// console.log(h);
+// // alert(h);
+// // document.write(h);
+//
+// let i = 3.14;
+// console.log(i);
+// // alert(i);
+// // document.write(i);
+//
+// let j = 2.7;
+// console.log(j);
+// // alert(j);
+// // document.write(j);
+//
+// let k = 16;
+// console.log(k);
+// // alert(k);
+// // document.write(k);
+//
+// let l = !0;
+// console.log(l);
+// // alert(l);
+// // document.write(l);
+//
+// let m = !!0;
+// console.log(m);
+// // alert(m);
+// // document.write(m);
+//
+// document.write(`<div> ${a} ${b} ${c} ${d} ${e} ${f} ${g} ${h} ${i} ${j} ${k} ${l} ${m} </div>`)
+
+
+// Second part
+// let book1 = {
+//     name: 'Doctor Sleep',
+//     pages: 645,
+//     genre: 'Fantacy'
+// }
+//
+// let book2 = {
+//     name: 'Sleeping Beauties',
+//     pages: 957,
+//     genre: 'Fantacy',
+//     autors: [
+// {name: 'Stephen King'},
+// {name:'Oven King'},
+//     ]
+// }
+// // console.log(book2.autors[1].name);
+
+
+// //Third part
+// let firstName = 'Юля';
+// let middleName = 'Любомирівна';
+// let lastName = 'Гаврилишин';
+// console.log(lastName +' ' + firstName + ' ' + middleName);
+
+// //Forth part
+// let user = {name:'Юля',middleName:'Любомирівна',age:23};
+//
+// let field = prompt(user);
+// console.log(user[field]);
+
+
+// // Fifth part
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+
+
+// /*Last part*/
+//
+// let books = [
+// {name: 'Doctor Sleep', pages: 645, genre: 'Fantacy', autor:'Stephen King'},
+// {name: 'Sleeping Beauties', pages: 957, genre: 'Fantacy', autor:'Stephen King'}
+// ]
+// console.log(books[0].name);
+// console.log(books[1].name);
