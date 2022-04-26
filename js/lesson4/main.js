62b3b94d5df3f46b9344bcc04e5408bd9d23d68a
Zoella7 <yuliahavrylyshyn1@gmail.com>
2022-04-26 13:29:52 +0300

    hw4

---
+++ js/lesson4/main.js
@@ -0,0 +1,115 @@
+// function squareP(a, b) {
+//     let result = (a +b)* 2;
+//     return result;
+// }
+//  let answer = squareP(20, 15);
+// console.log(answer);
+
+// function circle(r) {
+//     let result = ( 3,14 * (r * r));
+//         return result;
+//
+// }
+// let area = circle(12);
+// console.log(area);
+
+// function cylinder(r, h) {
+//     let result =(2 * 3,14 * r * h);
+//     return result
+//     }
+//     let answer = cylinder(5, 9);
+// console.log(answer);
+
+// function mainM(massif) {
+//     for (let part of massif) {
+//         console.log(part);
+//            }
+//     }
+//
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// mainM(listOfItems);
+
+// function smth(text) {
+//     document.write(`<p> ${text} </p>`);
+//     }
+//     smth('Good morning!!!');
+
+// function main(text) {
+//     document.write(`<ul>`);
+//     document.write(`<li> ${text} </li>`);
+//     document.write(`<li> ${text} </li>`);
+//     document.write(`<li> ${text} </li>`);
+//     document.write(`</ul>`);
+// }
+//
+// main('some words');
+
+
+// function main(size) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < size; i++) {
+//         document.write(`<li> some text </li>`);
+//            }
+//     document.write(`</ul>`);
+// }
+//
+// main(3);
+
+
+// function usersID(array) {
+//     for (const Element of array) {
+//         console.log(Element);
+//     }
+// }
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// usersID(users);
+
+
+// function date(arra) {
+//     for (let item of arra) {
+//         document.write(`<div> ${item.id} ${item.name} ${item.age}</div>`);
+//
+//     }
+//     }
+//
+// let usersList = [
+//     {
+//         id: 1,
+//         name: 'Leanne Graham',
+//         age: 23
+//              },
+//     {
+//         id: 2,
+//         name: 'Ervin Howell',
+//         age: 43
+//             },
+//     {
+//         id: 3,
+//         name: 'Stiphen Kind',
+//         age: 66
+//     }
+//   ];
+//
+//  date(usersList);
+
+
+// function sum(a, b, c) {
+//      return (a + b + c);
+// }
+// let result = sum(2, 3, 2);
+// console.log(result);
+
+
